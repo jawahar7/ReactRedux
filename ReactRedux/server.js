bddd9374f517880650bd3457912d10af70f1d6fe
jawahar7.ceg <jawahar7.ceg@gmail.com>
2016-08-04 22:54:21 +0530

    run react app

--- ReactRedux/server.js
+++ ReactRedux/server.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
+//Middleware
+app.use(express.static('./dist'));
+
 app.get('/', function(req, res){
-	res.send('hai from server');
+	res.sendFile(path.resolve('client/index.html'));
 });
 
 app.listen('3000', function(err){
